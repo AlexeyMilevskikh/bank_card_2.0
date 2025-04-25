047a0c3f748213886a24a099c40f27dc07b57a7d
Alexey Milevskikh <alexeyshvay@icloud.com>
2025-04-25 18:43:33 +0300

    add operations function

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,5 @@
 import pytest
-from src.processing.operations import filter_by_state
+from src.processing.operations import filter_by_state, sort_by_date
 
 
 @pytest.fixture
@@ -26,3 +26,30 @@ def test_filter_by_state_canceled(sample_operations):
 
 def test_filter_by_state_empty_result(sample_operations):
     assert filter_by_state(sample_operations, "UNKNOWN") == []
+
+
+def test_sort_by_date_descending():
+    ops = [
+        {'id': 1, 'date': '2022-01-01T00:00:00.000000'},
+        {'id': 2, 'date': '2023-01-01T00:00:00.000000'},
+        {'id': 3, 'date': '2021-01-01T00:00:00.000000'}
+    ]
+    result = sort_by_date(ops)
+    assert [op['id'] for op in result] == [2, 1, 3]
+
+def test_sort_by_date_ascending():
+    ops = [
+        {'id': 1, 'date': '2022-01-01T00:00:00.000000'},
+        {'id': 2, 'date': '2023-01-01T00:00:00.000000'},
+        {'id': 3, 'date': '2021-01-01T00:00:00.000000'}
+    ]
+    result = sort_by_date(ops, reverse=False)
+    assert [op['id'] for op in result] == [3, 1, 2]
+
+def test_sort_by_date_with_missing_key():
+    ops = [
+        {'id': 1, 'date': '2022-01-01T00:00:00.000000'},
+        {'id': 2}  # Нет ключа date
+    ]
+    with pytest.raises(KeyError):
+        sort_by_date(ops)
